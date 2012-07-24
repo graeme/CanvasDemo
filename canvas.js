1adc3716ba7c87bc7615652d6cda7517334ba6c6
Graeme Arthur <graeme@graemearthur.com>
2012-07-24 13:09:09 +0100

    Drawn a smiley face

--- canvas.js
+++ canvas.js
@@ -1,9 +1,42 @@
+var canvas;
+var context;
+
 window.onload = function()
 {
-	var canvas = document.getElementById('canvas');
-	var context = canvas.getContext('2d');
+	canvas = document.getElementById('canvas');
+	context = canvas.getContext('2d');
+	
+	drawCurve(100, 100, 150, 140, 200, 100);
+	drawCurve(140, 60, 150, 80, 160, 60);
+	drawCircle(120, 20, 5)
+	drawCircle(180, 20, 5)
+}
 
-	context.fillStyle = 'red';
-	// default height is 150 and width 300
-	context.fillRect(100, 50, 100, 50);
+function fillSquare(colour, x, y, width) {
+	
+	context.fillStyle = colour;
+	context.fillRect(x, y, width, width);
+	
+}
+
+function drawCurve(startx, starty, cpx, cpy, endx, endy) {
+	
+	// 'startx' and 'starty' = start of the line
+	// 'cpx' and 'cpy' = curve point
+	// 'endx' and 'endy' = end of the line
+	
+	context.moveTo(startx, starty);
+	context.quadraticCurveTo(cpx, cpy, endx, endy);
+	context.stroke();
+}
+
+function drawCircle(posx, posy, radius) {
+	
+	// 'posx' and 'posy' = position of circle
+	// 'radius' = radius of circle
+	
+	context.beginPath();
+	context.arc(posx, posy, radius, 0, Math.PI*2, true);
+	context.closePath();
+	context.fill();
 }
